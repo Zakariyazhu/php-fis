8cc29c0cd8aa0444145242502edcdf975d2de57e
Zakariyazhu <zkaipm2@163.com>
2016-04-12 11:25:58 +0800

    Update config_api_params.inc.php

--- php/config/config_api_params.inc.php
+++ php/config/config_api_params.inc.php
@@ -1,16 +1,13 @@
 <?php
 $apiConfig = [
     'domain' => 'www.fkls.com',
-    //'api_host' => 'http://192.168.0.229:9090/entstudy-api',
-    //'api_host' => 'http://192.168.0.229:8080/entstudy-api',
-    'api_host' => 'http://test.api.entstudy.com',
-    'packet_api_host' => 'http://pay.api.entstudy.com',
-    //http://10.0.0.36/F2E/web-fkls/css/base/common.css
-    'static_host' => 'http://10.0.0.36/F2E/web-fkls/',
+    'api_host' => 'http://test.api.com',
+    'packet_api_host' => 'http://pay.api.test.com',
+    'static_host' => 'http://127.0.0.1/F2E/web-fkls/',
     'version' => '1.0.0',
-    "site_name" => '疯狂老师',
-    'API_MD5KEY' => 'pIWS1fRknjsgznoCyJHxdd8OL855Q2Kh',
-    'API_RES_KEY' => 'JadK34ZtQDI0lP7cSupU0DKP',
+    "site_name" => '123',
+    'API_MD5KEY' => '',
+    'API_RES_KEY' => '',
     // 公共query参数
-    'api_queryStr' => 'ver=4.2.3&plat=ios&ch=entstudy&from=s&osver=8.1.2&deviceid=31594F9E-0CF1-49F1-989F-62B5C5367333&network=wifi&lat=39.993582&lng=116.323170&cityid=100',
+    'api_queryStr' => '',
 ];
