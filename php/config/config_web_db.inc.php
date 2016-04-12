3170818cea5c73c98b8fe153bcd3a63f54a526fe
Zakariyazhu <zkaipm2@163.com>
2016-04-12 11:26:23 +0800

    Update config_web_db.inc.php

--- php/config/config_web_db.inc.php
+++ php/config/config_web_db.inc.php
@@ -1,6 +1,5 @@
 <?php
 $db_handle_name = 'm_db';
 $dbconfig = array(
-    'master'=>array('host'=>'192.168.0.244','user'=>'root','pass'=>'123456','dbname'=>'m','dbport'=>'3306'),
-    'slave'=>array('host'=>'192.168.0.244','user'=>'root','pass'=>'123456','dbname'=>'m','dbport'=>'3306'),
+    'master'=>array('host'=>'192.168.0.244','user'=>'root','pass'=>'123456','dbname'=>'test','dbport'=>'3306'),
 );
